7d396e478235053778240ba153f59bb126fed115
Maverick Perrollaz <maverick.perrollaz@pm.me>
2021-03-03 17:24:55 +0100

    add method Huffman.make_frequency()

--- huffman/Huffman.py
+++ huffman/Huffman.py
@@ -10,6 +10,16 @@ class Huffman():
       self.compress(text)
 
    def compress(self, text):
-      print(text)
-      pass
+      frequency = self.make_frequency(text)
+      print(frequency)
+
+   def make_frequency(self, text):
+      frequency = {}
+      for character in text:
+         if character in frequency:
+            frequency[character] += 1
+         else:
+            frequency[character] = 1
+
+      return dict(sorted(frequency.items(), key=lambda x: x[1]))
 
