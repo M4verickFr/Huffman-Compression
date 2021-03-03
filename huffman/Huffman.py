f188d5790a35c7c57e1916130d4793700c73db0f
Maverick Perrollaz <maverick.perrollaz@pm.me>
2021-03-03 17:32:56 +0100

    add method Huffman.make_tree()

--- huffman/Huffman.py
+++ huffman/Huffman.py
@@ -1,4 +1,5 @@
 from .File import File
+from .Tree import Node
 
 """class to encode data into smaller bits according to their 
    frequinces of occurance.
@@ -11,7 +12,8 @@ class Huffman():
 
    def compress(self, text):
       frequency = self.make_frequency(text)
-      print(frequency)
+      root_node = self.make_tree(frequency)
+      print(root_node)
 
    def make_frequency(self, text):
       frequency = {}
@@ -22,4 +24,18 @@ class Huffman():
             frequency[character] = 1
 
       return dict(sorted(frequency.items(), key=lambda x: x[1]))
+
+   def make_tree(self, frequency):
+   	  # convert each characters in Node
+      list_nodes = [Node(weight, character) for character,weight in frequency.items()]
+
+      while len(list_nodes)>1:
+         list_nodes = sorted(list_nodes, key=lambda x: x.get_weight())
+         left_node = list_nodes.pop(0)
+         right_node = list_nodes.pop(0)
+         parent_node = Node(left_node.get_weight() + right_node.get_weight())
+         parent_node.setChildren(left_node, right_node)
+         list_nodes.append(parent_node)
+
+      return list_nodes[0]
 
