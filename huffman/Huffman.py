e9ca4ba4c78da86b1c958c27696d1ba80ffac399
Maverick Perrollaz <maverick.perrollaz@pm.me>
2021-03-03 16:45:11 +0100

    inital files to make package

---
+++ huffman/Huffman.py
@@ -0,0 +1,10 @@
+"""class to encode data into smaller bits according to their 
+   frequinces of occurance.
+"""
+class Huffman():
+   def compressFile(self, path):
+      pass
+
+   def compress(self, text):
+      pass
+
